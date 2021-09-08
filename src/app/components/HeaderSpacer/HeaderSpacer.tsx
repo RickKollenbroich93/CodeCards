d15e7c5b0ead011cb2410b6b2c38fdcb8ccb4f57
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-09-08 10:53:44 +0200

    ✨  Create new React object for my Header

---
+++ src/app/components/HeaderSpacer/HeaderSpacer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './HeaderSpacer.module.css';
+
+function HeaderSpacer(): JSX.Element {
+  return <span className={styles.headerSpacer}>#</span>;
+}
+
+export default HeaderSpacer;
