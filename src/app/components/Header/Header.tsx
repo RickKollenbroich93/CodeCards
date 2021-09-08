d6ab0de7a38f95a1f352e583f833a1ae66d43b68
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-09-08 10:06:49 +0200

    ✨ Create header component

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import type { ReactNode } from 'react';
+import React from 'react';
+import styles from './Header.module.css';
+
+type HeaderProps = {
+  children?: ReactNode;
+  className?: string;
+};
+
+function Header({ children, className }: HeaderProps): JSX.Element {
+  return (
+    <header
+      className={`${styles.headerContainer} ${className}
+      }`}
+    >
+      {' '}
+      <span className={styles.headerBracket}> &lt; </span>
+      <h1 className={styles.header}>{children}</h1>
+      <span className={styles.headerBracket}> &gt; </span>
+    </header>
+  );
+}
+
+export default Header;
