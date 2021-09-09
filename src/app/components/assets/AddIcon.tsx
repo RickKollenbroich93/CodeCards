d3bac35e0279de47f0f287a31eba1af32ae39b09
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-09-09 14:49:58 +0200

    💄 Config SVGs for active and inactive stats

--- src/app/components/assets/AddIcon.tsx
+++ src/app/components/assets/AddIcon.tsx
@@ -6,13 +6,12 @@ const AddIcon = (props: React.SVGProps<SVGSVGElement>): JSX.Element => {
       width="21"
       height="20"
       viewBox="0 0 21 20"
-      fill="none"
+      stroke="white"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
         d="M11 10H1M11 20V10V20ZM11 10V0V10ZM11 10H21H11Z"
-        stroke="white"
         stroke-width="2"
         stroke-linecap="round"
       />
