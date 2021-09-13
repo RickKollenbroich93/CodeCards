3b75449b0c8634d312f1ea96d8113828ecadaca1
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-09-13 18:58:17 +0200

    💄 Style gaps between buttons

--- src/app/components/CodeCard/CodeCard.tsx
+++ src/app/components/CodeCard/CodeCard.tsx
@@ -4,9 +4,12 @@ import styles from './CodeCard.module.css';
 import '../../prism.css';
 import WindowStateButton from '../Buttons/WindowStateButton/WindowStateButton';
 import FullScreenIcon from '../assets/FullScreenIcon';
+import DeleteIcon from '../assets/DeleteIcon';
+import EditIcon from '../assets/EditIcon';
 import TagGroup from '../TagGroup/TagGroup';
 import SubHeader from '../Header/SubHeader/SubHeader';
 import DescriptionBox from '../DescriptionBox/DescriptionBox';
+import MinScreenIcon from '../assets/MinScreenIcon';
 
 export type CardProps = {
   layout?: 'compact' | 'detail';
@@ -46,13 +49,31 @@ export default function CodeCard({
             <code className={`language-${language}`}>{content}</code>
           </pre>
         </>
-
-        <WindowStateButton>
-          <FullScreenIcon />
-        </WindowStateButton>
+        {layout === 'compact' && (
+          <WindowStateButton>
+            <FullScreenIcon />
+          </WindowStateButton>
+        )}
+        {layout === 'detail' && (
+          <div className={styles.buttonWrapper}>
+            <WindowStateButton>
+              <DeleteIcon />
+            </WindowStateButton>
+            <WindowStateButton>
+              <EditIcon />
+            </WindowStateButton>
+          </div>
+        )}
       </div>
       <TagGroup tagList={MockList} />
-      {layout === 'detail' && <DescriptionBox>{description}</DescriptionBox>}
+      {layout === 'detail' && (
+        <>
+          <DescriptionBox>{description}</DescriptionBox>{' '}
+          <WindowStateButton>
+            <MinScreenIcon />
+          </WindowStateButton>
+        </>
+      )}
     </div>
   );
 }
