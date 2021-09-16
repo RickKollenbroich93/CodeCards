c407a28c3f62c957e3dd6d4289bb0a280c2c999a
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-09-16 08:11:16 +0200

    ✨ Make it possible to have no tags in a taggroup

--- src/app/components/TagGroup/TagGroup.tsx
+++ src/app/components/TagGroup/TagGroup.tsx
@@ -6,23 +6,24 @@ import type { ReactNode } from 'react';
 type TagProps = {
   active?: boolean;
   onClick?: () => void;
-  children: ReactNode;
+  children?: ReactNode;
   className?: string;
 };
 
 export type TagGroupProps = {
-  tagList: TagProps[];
+  tagList?: TagProps[];
   className?: string;
 };
 
 const TagGroup = ({ tagList, className }: TagGroupProps): JSX.Element => {
   return (
     <section className={`${styles.container} ${className}`}>
-      {tagList.map((tag, index) => (
-        <Tag key={index} active={tag.active} onClick={tag.onClick}>
-          {tag.children}
-        </Tag>
-      ))}
+      {tagList !== undefined &&
+        tagList.map((tag, index) => (
+          <Tag key={index} active={tag.active} onClick={tag.onClick}>
+            {tag.children}
+          </Tag>
+        ))}
     </section>
   );
 };
