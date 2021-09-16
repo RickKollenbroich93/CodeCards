e462bfd4f0a76e8d1446e7021517c6646de4e38b
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-09-16 16:23:28 +0200

    ✨ Create component for adding Collection tags

--- src/app/components/CollectionTagGroup/CollectionTagGroup.tsx
+++ src/app/components/CollectionTagGroup/CollectionTagGroup.tsx
@@ -1,27 +1,74 @@
 import React from 'react';
-// import type { ReactNode } from 'react';
-
 import styles from './CollectionTagGroup.module.css';
 import TagGroup from '../TagGroup/TagGroup';
-
 import Tag from '../Tag/Tag';
+import { useState } from 'react';
+import Header from '../Header/Header';
+import AddButton from '../Buttons/AddButton/AddButton';
 
 type CollectionTagGroup = {
   children: string;
 };
 
-function SearchBar(): JSX.Element {
+function CollectionTags(): JSX.Element {
+  //All Collections
+  const allCollections: CollectionTagGroup[] = [];
+  const allCollectionList = allCollections.map((collection) => {
+    return { children: collection };
+  });
+  //All Collections END
+
+  //Selected Collections
   const selectedCollections: CollectionTagGroup[] = [];
   const selectedCollectionList = selectedCollections.map((collection) => {
     return { children: collection };
   });
+  //Selected Collections END
+
+  //Modal function
+  const [modalToggle, setModalToggle] = useState(false);
+  function handleModal() {
+    setModalToggle(!modalToggle);
+  }
+  const modal = document.querySelector('#modal');
+  modalToggle === false && modal?.classList.add(`${styles.modalHidden}`);
+  modalToggle === true && modal?.classList.remove(`${styles.modalHidden}`);
+  //Modal function END
+
+  //Mock SHIT
+  // const testList = [
+  //   { children: 'Hallo' },
+  //   { children: 'I hate this component' },
+  // ];
 
   return (
-    <div className={styles.collectionContainer}>
-      {selectedCollections && <TagGroup tagList={selectedCollectionList} />}
-      <Tag active>Select Collection</Tag>
-    </div>
+    <>
+      <div className={styles.collectionContainer}>
+        {selectedCollections && <TagGroup tagList={selectedCollectionList} />}
+        <Tag active onClick={() => handleModal()}>
+          Select Collection
+        </Tag>
+      </div>
+
+      <section className={styles.modal} id="modal">
+        TEXT
+        <div className={styles.modalContent}>
+          <Header className={styles.headerModal}>Select Collections</Header>
+          {allCollections && (
+            <TagGroup className={styles.tagList} tagList={allCollectionList} />
+          )}
+          {allCollectionList && (
+            <p className={styles.noCollections}>You have no Collections</p>
+          )}
+          <AddButton
+            type="button"
+            children="Done"
+            onClick={() => handleModal()}
+          />
+        </div>
+      </section>
+    </>
   );
 }
 
-export default SearchBar;
+export default CollectionTags;
