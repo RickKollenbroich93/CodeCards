65a38ad4208e0f6ec2afb2006e33e57b941399cc
Rick Kollenbroich <87307470+RickKollenbroich93@users.noreply.github.com>
2021-09-20 10:33:42 +0200

    Addpage (#28)
    * 💄 Give a height to tag component
    
    * ✨ Add Add-page to switch router in app.tsx
    
    * ✨ Create new add page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Collections from './pages/Collections/Collections';
+import AddCard from './pages/AddCard/AddCard';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/add">
+          <AddCard />
+        </Route>
         <Route path="/collections">
           <Collections />
         </Route>
