09d1da6f3a56ce6798243de3b11107f602945b4b
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-09-29 14:26:59 +0200

    💄  Add Scroller Animation

--- src/app/components/Scroller/Scroller.tsx
+++ src/app/components/Scroller/Scroller.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styles from './Scroller.module.css';
 
-export default function Scroller(): JSX.Element {
+type Scroller = {
+  className: string;
+};
+
+export default function Scroller({ className }: Scroller): JSX.Element {
   return (
-    <div className={styles.scrolldowns}>
-      <div className={styles.mousey}>
-        <div className={styles.scroller}></div>
+    <div
+      className={`${styles.headerContainer} ${className}
+  }`}
+    >
+      <div className={styles.scrolldowns}>
+        <div className={styles.mousey}>
+          <div className={styles.scroller}></div>
+        </div>
       </div>
     </div>
   );
